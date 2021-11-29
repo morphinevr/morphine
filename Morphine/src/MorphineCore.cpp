9f4bd6e68e3214f8ec9c8867c204fc303dd6219b
Hyblocker <bryan.spiteri@gmail.com>
2021-11-30 00:02:59 +0100

    Fix CMake not working on Winblows

--- Morphine/src/MorphineCore.cpp
+++ Morphine/src/MorphineCore.cpp
@@ -1,8 +1,14 @@
 #include "Morphine/Morphine.h"
+#include "psmoveapi.h"
 #include <iostream>
 
 namespace Morphine {
     void MorphineCore::Init() {
         std::cout << "Hello from MorphineAPI!" << std::endl;
+
+        // Setup PS Move API
+        psmoveapi_init(NULL, NULL);
+
+        psmoveapi_quit();
     }
 }
